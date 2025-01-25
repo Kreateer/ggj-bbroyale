aeabb6b47ed00d331188c73bc29b1c34d846d5b2
TheRealDemmy <Dendimir101@yahoo.com>
2025-01-25 12:29:16 +0100

    Added sprinting
    "Sprint" mechanics on ship added, base steerpower and power is 2000 and 5, and when sprinting it slowly increments to 1000 steerpower and 10 power. More fast, less manueverable.

--- Assets/Scripts/Player/DuckieMovement.cs
+++ Assets/Scripts/Player/DuckieMovement.cs
@@ -7,17 +7,23 @@ public class DuckieMovement : MonoBehaviour
 
     PlayerInput playerInput;
     InputAction moveAction;
+    InputAction sprintAction;
 
     public Transform Motor;
-    public float Steerpower = 3500f;
-    public float Power = 1f;
-    public float MaxSpeed = 10f;
-    public float Drag = 0.1f;
+    public float Steerpower;
+    public float Power;
+    public float MaxSpeed;
+    public float MinSpeed;
+    public float Drag;
+    public float AccelerationSpeed;
+    public float SteerAccelerationSpeed;
 
     protected Rigidbody Rigidbody;
     protected Quaternion StartRotation;
 
     Vector2 moveDirection = Vector2.zero;
+
+    public bool isMovingForward;
 
     public float xRot;
     public float zRot;
@@ -29,12 +35,15 @@ public class DuckieMovement : MonoBehaviour
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("Move");
+        sprintAction = playerInput.actions.FindAction("Sprint");
         Rigidbody = GetComponent<Rigidbody>();
         StartRotation = Motor.localRotation;
     }
 
     private void FixedUpdate()
     {
+
+        //Boat Controls
         var forceDirection = transform.forward;
         var steer = 0;
 
@@ -50,26 +59,53 @@ public class DuckieMovement : MonoBehaviour
         {
             steer = 1;
         }
-
+         //Left and Right turning
         Rigidbody.AddForceAtPosition(steer * transform.right * Steerpower / 100f, Motor.position);
 
+        //Back and Forth movement
         var forward = Vector3.Scale(new Vector3(1, 0, 1), transform.forward);
         var backwards = Vector3.Scale(new Vector3(-1, 0, -1), transform.forward);
 
         if (moveDirection.y > 0)
         {
             Rigidbody.AddForce(forward * Power, ForceMode.Impulse);
-          
         }
         else if (moveDirection.y < 0)
         {
             Rigidbody.AddForce(backwards * Power, ForceMode.Impulse);
         }
 
+        if (sprintAction.IsPressed() == true)
+        {
+            if (Power >= MaxSpeed)
+            {
+                Power = MaxSpeed;
+                Steerpower = 1000f;
+            }
+            else
+            {
+                Power = Power + (Time.deltaTime * AccelerationSpeed);
+                Steerpower = Steerpower - (Time.deltaTime * SteerAccelerationSpeed);
+            }
+          
+        }
+        else
+        {
+            if (Power <= MinSpeed)
+            {
+                Power = MinSpeed;
+                Steerpower = 2000f;
+            }
+            else
+            {
+                Power = Power - (Time.deltaTime * AccelerationSpeed);
+                Steerpower = Steerpower + (Time.deltaTime * SteerAccelerationSpeed);
+            }
+        }
+
+        //Fake Bobbing
         float xRot = (Mathf.Sin(Time.time * bobRotSpeed) * bobRotAmount);
-
         float zRot = (Mathf.Sin((Time.time - 1.0f) * bobRotSpeed) * bobRotAmount);
-
         transform.eulerAngles = new Vector3(xRot, transform.eulerAngles.y, zRot);
 
     }
