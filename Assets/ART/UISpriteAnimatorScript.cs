3849fd9fa4d78be951bfeaf6e3ed99cccb8117fe
777Xarina777 <xarina777@yandex.ru>
2025-01-26 00:12:01 +0100

    added ui and scrip animations in ui

---
+++ Assets/ART/UISpriteAnimatorScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISpriteAnimation : MonoBehaviour
+{
+
+    
+    public Image image;
+    public List<Sprite> sprites;
+    public float animSpeed = 1;
+    private int index;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartAnim());
+    }
+
+    IEnumerator StartAnim()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(animSpeed);
+            index++;
+            if (index >= sprites.Count)
+                index = 0;
+            else
+                image.sprite = sprites[index];
+        }
+    }
+   
+}
