471b0ad2d332c325ba6425a02f94b3292c6c0661
MiguelCR <miguelcroman11@gmail.com>
2025-01-25 21:27:37 +0100

    Life and death... boundaries

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,9 +15,22 @@ public class GameManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.B))
         {
-            Vector3 origin = new Vector3(Random.Range(-100, 100), -50, Random.Range(50, 150));
+            Vector3 origin = new Vector3(Random.Range(-110, 110), -50, Random.Range(-55, 55));
             Vector3 goal = player.transform.position;
             SpawnObject.Spawn(origin, goal, gos[Random.Range(0,1)]);
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            float randX = Random.Range(0, 150);
+            float randZ = Random.Range(0, 80);
+            if (randX < 120)
+                randZ = Random.Range(60, 80);
+            randX *= Random.Range(0, 2) * 2 - 1;
+            randZ *= Random.Range(0, 2) * 2 - 1;
+            //Vector3 origin = new Vector3(Random.Range(120, 150) * (Random.Range(0,2)*2-1), -50, Random.Range(60, 80) * (Random.Range(0, 2) * 2 - 1));
+            Vector3 origin = new Vector3(randX, -50, randZ);
+            Vector3 goal = player.transform.position;
+            SpawnObject.Spawn(origin, goal, gos[Random.Range(1, 3)]);
         }
     }
 }
